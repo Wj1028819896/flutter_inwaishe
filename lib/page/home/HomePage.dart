3dd961eacbd8249f7749ae3a99ae61a0e779e9bb
wangjing <1028819896@qq.com>
2019-07-19 18:47:17 +0800

    测试提交1

--- lib/page/home/HomePage.dart
+++ lib/page/home/HomePage.dart
@@ -18,10 +18,15 @@ class HomePage extends StatelessWidget{
         child: new Image.network("https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=e6821d0a831001e9513c120f880f7b06/a71ea8d3fd1f4134d244519d2b1f95cad0c85ee5.jpg",fit: BoxFit.fill),
         borderRadius: BorderRadius.circular(5.0));
 
+    Widget page4 = ClipRRect(
+        child: new Image.network("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=691137987,2953731347&fm=11&gp=0.jpg",fit: BoxFit.fill),
+        borderRadius: BorderRadius.circular(5.0));
+
     pagers
       ..add(new Container(margin: EdgeInsets.all(10),child: page1))
       ..add(new Container(margin: EdgeInsets.all(10),child: page2))
-      ..add(new Container(margin: EdgeInsets.all(10),child: page3));
+      ..add(new Container(margin: EdgeInsets.all(10),child: page3))
+      ..add(new Container(margin: EdgeInsets.all(10),child: page4));
     return new ConstrainedBox(constraints: BoxConstraints(maxHeight: 250),
         child: new CommonBanner(pagers, 1000, false));
   }
